3f1a96e5fcca9c35b587c2b32cdf83b1636219b2
Lucas281182 <134404999+Lucas281182@users.noreply.github.com>
2024-06-17 10:52:48 -0300

    Add files via upload

--- Lucas Mariano/scripts/player.gd
+++ Lucas Mariano/scripts/player.gd
@@ -1,16 +1,36 @@
 extends Area2D
 
-	const SPEED := 400
-# Called when the node enters the scene tree for the first time.
+const SPEED := 400
+@onready var screen_size = get_viewport_rect().size
+@onready var anim = $anim
+@onready var collision = $collision
+	# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		
+	if velocity.x != 0:
+		anim.play("move") 
+	elif velocity.y > 0:
+		anim.play ("move up")
+	elif velocity.y < 0:
+		anim.play("move down")
+	else:
+		anim.play("idle")
+	#if velocity.x > 0:
+		#anim.flip_h = true
+	#else:
+		#anim.flip_h = false
+	
+	anim.flip _h = true if velocity.x > 0 else false
+	
+	
 	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
